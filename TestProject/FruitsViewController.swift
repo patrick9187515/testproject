da297aeedf494f59ab2cfe0c4a7c8746d4a5380f
pat t <pat@pats-iMac.local>
2017-04-05 08:34:40 -0700

    update

---
+++ TestProject/FruitsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  FruitsViewController.swift
+//  TestProject
+//
+//  Created by pat t on 4/5/17.
+//  Copyright © 2017 Footy Headlines. All rights reserved.
+//
+
+import UIKit
+
+class FruitsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath)
+        
+        cell.textLabel?.text = "Section \(indexPath.section) Row \(indexPath.row)"
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Section \(section)"
+    }
+
+}
