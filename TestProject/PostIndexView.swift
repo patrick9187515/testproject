8b880897da85d94d0fbc9c98db3e3a4d7bf97161
pat t <pat@pats-iMac.local>
2017-04-17 18:43:52 -0700

    .

---
+++ TestProject/PostIndexView.swift
@@ -0,0 +1,24 @@
+//
+//  PostIndexView.swift
+//  TestProject
+//
+//  Created by pat t on 4/17/17.
+//  Copyright © 2017 Footy Headlines. All rights reserved.
+//
+
+import UIKit
+
+class PostIndexView: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
