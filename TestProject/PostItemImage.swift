58fad4c0399f7b1f4a9f383fef84b5f25aac693b
pat t <pat@pats-iMac.local>
2017-04-20 11:54:36 -0700

    fixes

---
+++ TestProject/PostItemImage.swift
@@ -0,0 +1,25 @@
+//
+//  PostItemImage.swift
+//  TestProject
+//
+//  Created by pat t on 4/20/17.
+//  Copyright © 2017 Footy Headlines. All rights reserved.
+//
+
+import UIKit
+
+class PostItemImage: UITableViewCell {
+    @IBOutlet weak var postItemImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- TestProject/PostItemViewController.swift
+++ TestProject/PostItemViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import Foundation
+
+struct Image {
+    let url: String
+    var height: Int
+    let width: Int
+}
+
+struct Paragraph {
+    let content: String
+    var images: [Image]
+    let type: String
+}
+
+extension Paragraph {
+    init?(content: String) {
+        self.content = content
+        self.images = [Image]()
+        self.type = "text"
+    }
+}
 
 class PostItemViewController: UITableViewController {
     @IBOutlet weak var postTitle: UILabel!
-    @IBOutlet weak var stackView: UIStackView!
     
     var post : Post?
+    var paragraphs = [Paragraph]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,23 +42,121 @@ class PostItemViewController: UITableViewController {
         
         // Do any additional setup after loading the view.
         
-        postTitle.text = "test"
-        postTitle.text = post?.title
+        //postTitle.text = "test"
+        //postTitle.text = post?.title
         
-        let paragraphs = getParagraphs(content: (post?.content)!)
+        paragraphs = getParagraphs(content: (post?.content)!)
         
-        for paragraph in paragraphs {
-            let label = UILabel()
-            label.text = paragraph
-            label.translatesAutoresizingMaskIntoConstraints = false
-            stackView.addArrangedSubview(label)
-        }
+        tableView.register(UINib(nibName: "PostItemText", bundle: nil),
+                           forCellReuseIdentifier: "PostTextCell")
+        
+        tableView.register(UINib(nibName: "PostItemImage", bundle: nil),
+                           forCellReuseIdentifier: "PostImageCell")
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 100
     }
     
-    func getParagraphs(content: String) -> Array<String> {
-        let paragraphs = content.components(separatedBy: "<br />")
+
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if paragraphs[indexPath.row].type == "image" {
+            //return CGFloat(paragraphs[indexPath.row].images[0].height)
+        }
+        return UITableViewAutomaticDimension
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return paragraphs.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var paragraph = paragraphs[indexPath.row]
+        //var cell
         
-        return paragraphs
+        if (paragraph.type == "image") {
+            let imageUrl = paragraph.images[0].url
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath) as! PostItemImage
+            
+            //cell.postItemImageView.sd_setImage(with: URL(string: imageUrl))
+            cell.postItemImageView.sd_setImage(with: URL(string: imageUrl), completed: { (image, error, imageCacheType, imageUrl) in
+                paragraph.images[0].height = 20
+                let mHeight = image?.size.height
+                let mWidth = image?.size.width
+                let newHeight = 320 * (mHeight! / mWidth!)
+                
+                paragraph.images[0].height = Int(newHeight)
+                
+                cell.postItemImageView.frame = CGRect(x: 0, y: 0, width: 320, height: newHeight)
+                
+                //let constraint = NSLayoutConstraint(item: cell.postItemImageView, attribute: NSLayoutAttribute.height, relatedBy: <#T##NSLayoutRelation#>, toItem: <#T##Any?#>, attribute: <#T##NSLayoutAttribute#>, multiplier: <#T##CGFloat#>, constant: <#T##CGFloat#>)
+                
+                //cell.postItemImageView.addConstraint(constraint)
+                
+                print(newHeight)
+            })
+            
+            return cell
+        } else {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostTextCell", for: indexPath) as! PostItemText
+        
+        if paragraphs.count > indexPath.row {
+            cell.label.text = paragraphs[indexPath.row].content
+            
+            return cell
+        }
+        }
+        
+        return UITableViewCell()
+    }
+    
+    func getImageUrls(content: String) -> Array<Image> {
+        var results = [Image]()
+        
+        let regex = try! NSRegularExpression(pattern: "<img[^>]+src=\"([^\"]+)", options: [])
+        
+        let matches = regex.matches(in: content, options: [], range: NSMakeRange(0, content.characters.count))
+        
+        guard let match = matches.first else { return results }
+        
+        let lastRangeIndex = (match.numberOfRanges) - 1
+        
+        for i in 1...lastRangeIndex {
+            let capturedGroupIndex = match.rangeAt(i)
+            let matchedString = (content as NSString).substring(with: capturedGroupIndex)
+            results.append(Image(url: matchedString, height: 200, width: 0))
+        }
+        
+        return results
+    }
+    
+    func getParagraphs(content: String) -> Array<Paragraph> {
+        let parts = content.components(separatedBy: "<br />")
+        var matches = [Paragraph]()
+        
+        for part in parts {
+            let regex = try! NSRegularExpression(pattern: "[A-Za-z0-9]", options: [])
+        
+            if regex.numberOfMatches(in: part, options: [], range: NSRange(location: 0, length: part.characters.count)) > 0 {
+                if (part.range(of: "<img") != nil) && !(part.range(of: "hiddenImg") != nil) {
+                    matches.append(Paragraph(content: part, images: getImageUrls(content: part), type: "image"))
+                } else {
+                    matches.append(Paragraph(content: part)!)
+                }
+            }
+        }
+        
+        return matches
     }
 
     override func didReceiveMemoryWarning() {
